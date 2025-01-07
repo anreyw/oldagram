49a693ed9890555495fe9da0bd680daf00b4ad96
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-01-07 14:02:37 -0800

    added additional content and initial draft of like button

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+// TODO: Figure out how to render the data below to HTML
+// TODO: Figure out to how set up Like button to work for any post
 
+// const posts = [
+//     {
+//         name: "Vincent van Gogh",
+//         username: "vincey1853",
+//         location: "Zundert, Netherlands",
+//         avatar: "images/avatar-vangogh.jpg",
+//         post: "images/post-vangogh.jpg",
+//         comment: "just took a few mushrooms lol",
+//         likes: 21
+//     },
+//     {
+//         name: "Gustave Courbet",
+//         username: "gus1819",
+//         location: "Ornans, France",
+//         avatar: "images/avatar-courbet.jpg",
+//         post: "images/post-courbet.jpg",
+//         comment: "i'm feelin a bit stressed tbh",
+//         likes: 4
+//     },
+//         {
+//         name: "Joseph Ducreux",
+//         username: "jd1735",
+//         location: "Paris, France",
+//         avatar: "images/avatar-ducreux.jpg",
+//         post: "images/post-ducreux.jpg",
+//         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+//         likes: 152
+//     }
+// ]
+
+const likeButton = document.getElementById("like-btn")
+const likesCount = document.getElementById("likes-count")
+const postImg = document.getElementById("post-vangogh")
+
+likeButton.addEventListener("click", function() {
+    likesCount.innerHTML = "21,493 likes"
+})
+
+postImg.addEventListener("dblclick", function() {
+    likesCount.innerHTML = "22 likes"
+})
